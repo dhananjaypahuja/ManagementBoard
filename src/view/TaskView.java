65c3a21f602c5bdb4fc6ece529d94d6d6e30d910
Gregory Gan <gregory.t.gan@gmail.com>
2018-05-07 12:56:04 -0700

    TaskView constructor dependency

--- src/view/TaskView.java
+++ src/view/TaskView.java
@@ -1,22 +1,30 @@
 package view;
 
 import model.*;
+import java.awt.Component;
 import javax.swing.*;
 
 public class TaskView extends JPanel {
     private TaskModel task;
     private JLabel title, description, due;
 
-    public TaskView(TaskModel task) {
-        super(true);
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        this.task = task;
-        title = new JLabel(task.getTitle(), JLabel.CENTER);
-        description = new JLabel(task.getDescription(), JLabel.LEFT);
-        due = new JLabel(task.getDue().toString(), JLabel.CENTER);
+    public TaskView() {
+        super(new GridLayout(3, 1, 2, 1), true);
+        setBorder(BorderFactory.createLineBorder(Color.black));
+        title = new JLabel("New Task", JLabel.CENTER);
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
         add(title);
+        description = new JLabel("Description", JLabel.LEFT);
+        description.setAlignmentX(Component.LEFT_ALIGNMENT);
         add(description);
+        due = new JLabel("Date", JLabel.CENTER);
+        due.setAlignmentX(Component.CENTER_ALIGNMENT);
         add(due);
+
+    }
+    public TaskView(TaskModel task) {
+        this();
+        setTask(task);
     }
 
     public void setTask(TaskModel task) {
