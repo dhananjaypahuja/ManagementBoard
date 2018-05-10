c9572c0cc7f334af1b430c85d7b71cbd36db41d2
Gregory Gan <gregory.t.gan@gmail.com>
2018-05-09 18:54:21 -0700

    one way to use JScrollPane

--- src/view/ColumnView.java
+++ src/view/ColumnView.java
@@ -11,6 +11,7 @@ import javax.swing.border.*;
 public class ColumnView extends JPanel {
     // Reference to column data
     private ColumnModel column;
+    private JPanel subpanel;
 
     public ColumnView() {
         this(new ColumnModel());
@@ -28,14 +29,35 @@ public class ColumnView extends JPanel {
 
     public void setColumn(ColumnModel column) {
         this.column = column;
-        this.removeAll();
+        if (getComponentCount() > 0 && getComponent(0) instanceof JLabel)
+            remove(0);
         JLabel title = new JLabel(column.getTitle());
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
-        add(title);
+        add(title, 0);
+        if (getComponentCount() == 1)
+            add(new JScrollPane());
+        subpanel = new JPanel();
+        subpanel.setLayout(new BoxLayout(subpanel, BoxLayout.Y_AXIS));
         for (TaskModel task : column)
-            add(new TaskView(task));
+            subpanel.add(new TaskView(task));
+        ((JScrollPane) getComponent(1)).setViewportView(subpanel);
+        subcomponents(this, 1);
     }
     public ColumnModel getColumn() {
         return column;
+    }
+
+    public JPanel getSubpanel() {
+        return subpanel;
+    }
+
+    private static void subcomponents(Container c, int spaces) {
+        for (Component d : c.getComponents()) {
+            for (int i = 0; i < spaces; i ++)
+                System.out.print("  ");
+            System.out.println(d.toString());
+            if (d instanceof Container)
+                subcomponents((Container) d, spaces + 1);
+        }
     }
 }
