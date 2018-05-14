3fafaefb307c3ffa63795b8bc1c6b195b8b5e678
dhananjaypahuja <pahuja.dhananjay@gmail.com>
2018-05-14 13:35:08 -0700

    test columns

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -29,8 +29,14 @@ public class Main {
         JButton logOutButton = new JButton("Log Out");
 
         ProjectView projView = new ProjectView();//gridx 0 gridy 1
-        projView.setMinimumSize(new Dimension(128, 128));
-        projView.setProject(new ProjectModel());
+        TaskModel tModel1a = new TaskModel("Task 1a", "Description 1a", new Date());
+        TaskModel tModel1b = new TaskModel("Task 1b", "Description 1b", new Date());
+        TaskModel tModel2a = new TaskModel("Task 2a", "Description 2a", new Date());
+        TaskModel tModel2b = new TaskModel("Task 2b", "Description 2b", new Date());
+        ColumnModel cModel1 = new ColumnModel("Column 1", tModel1a, tModel1b);
+        ColumnModel cModel2 = new ColumnModel("Column 2", tModel2a, tModel2b);
+        ProjectModel pModel = new ProjectModel("Project", cModel1, cModel2);
+        projView.setProject(pModel);
 
         //specify diff grid x (changes) 0-6 grid y is 0
         c.gridx = 0;
