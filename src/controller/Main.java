49f4936d2d04e574da8d92584a9912f8ce8c9b2c
dhananjaypahuja <pahuja.dhananjay@gmail.com>
2018-05-15 11:17:09 -0700

    View class

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -4,6 +4,8 @@ import model.*;
 import view.*;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.awt.*;
 import java.util.Date;
@@ -22,14 +24,57 @@ public class Main {
 
         JLabel pLabel = new JLabel("Select Project");
         JScrollPane pScroll = new JScrollPane(); //Project list scroll
+
         JButton loadButton = new JButton("Load");
+        loadButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Load", "Test Box", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
         JButton editButton = new JButton("Edit");
+        editButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Edit", "Test Box", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
         JButton deleteButton = new JButton("Delete");
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Delete", "Test Box", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
         JButton saveButton = new JButton("Save");
-        JButton newPButton = new JButton("Create New");
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Save", "Test Box", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
+        JButton newProjButton = new JButton("Create New");
+        newProjButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Create New", "Test Box", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
         JButton logOutButton = new JButton("Log Out");
+        logOutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Log Out", "Test Box", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
 
         ProjectView projView = new ProjectView();//gridx 0 gridy 1
+
         TaskModel tModel1a = new TaskModel("Task 1a", "Description 1a", new Date());
         TaskModel tModel1b = new TaskModel("Task 1b", "Description 1b", new Date());
         TaskModel tModel2a = new TaskModel("Task 2a", "Description 2a", new Date());
@@ -46,7 +91,7 @@ public class Main {
         c.gridx = 1;
         jPanel.add(pScroll, c);
         c.gridx = 2;
-        jPanel.add(newPButton, c);
+        jPanel.add(newProjButton, c);
         c.gridx = 3;
         jPanel.add(saveButton, c);
         c.gridx = 4;
@@ -68,47 +113,48 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
-    
-    public static void dumbExample() {
-        // Set up components
-        JFrame frame = new JFrame("Test Frame");
-        TaskModel tModel1a = new TaskModel("Task 1a", "Description 1a", new Date());
-        TaskModel tModel1b = new TaskModel("Task 1b", "Description 1b", new Date());
-        TaskModel tModel2a = new TaskModel("Task 2a", "Description 2a", new Date());
-        TaskModel tModel2b = new TaskModel("Task 2b", "Description 2b", new Date());
-        ColumnModel cModel1 = new ColumnModel("Column 1", tModel1a, tModel1b);
-        ColumnModel cModel2 = new ColumnModel("Column 2", tModel2a, tModel2b);
-        ProjectModel pModel = new ProjectModel("Project", cModel1, cModel2);
-        ProjectView pView = new ProjectView(pModel);
-
-        // Put everything in the frame
-        frame.add(pView);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setBounds(0, 0, 384, 256);
-        frame.setVisible(true);
-
-        for (int i = 0; i < 10; i ++) {
-            try {
-                Thread.sleep(500);
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-            // Demonstrate column swapping
-            Manager.swapColumns(pView, 0, 1);
-            try {
-                Thread.sleep(250);
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-            // Demonstrate task swapping
-            Manager.swapTasks((ColumnView) pView.getComponent(0), 0, 1);
-        }
-
-        try {
-            FileIO.write("./example.proj", pModel);
-            System.out.println(FileIO.read("./example.proj").toString());
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
+    
+//    public static void dumbExample() {
+//        // Set up components
+//        JFrame frame = new JFrame("Test Frame");
+//        TaskModel tModel1a = new TaskModel("Task 1a", "Description 1a", new Date());
+//        TaskModel tModel1b = new TaskModel("Task 1b", "Description 1b", new Date());
+//        TaskModel tModel2a = new TaskModel("Task 2a", "Description 2a", new Date());
+//        TaskModel tModel2b = new TaskModel("Task 2b", "Description 2b", new Date());
+//        ColumnModel cModel1 = new ColumnModel("Column 1", tModel1a, tModel1b);
+//        ColumnModel cModel2 = new ColumnModel("Column 2", tModel2a, tModel2b);
+//        ProjectModel pModel = new ProjectModel("Project", cModel1, cModel2);
+//        ProjectView pView = new ProjectView(pModel);
+//
+//        // Put everything in the frame
+//        frame.add(pView);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setBounds(0, 0, 384, 256);
+//        frame.setVisible(true);
+//
+//        for (int i = 0; i < 10; i ++) {
+//            try {
+//                Thread.sleep(500);
+//            } catch(Exception e) {
+//                e.printStackTrace();
+//            }
+//            // Demonstrate column swapping
+//            Manager.swapColumns(pView, 0, 1);
+//            try {
+//                Thread.sleep(250);
+//            } catch(Exception e) {
+//                e.printStackTrace();
+//            }
+//            // Demonstrate task swapping
+//            Manager.swapTasks((ColumnView) pView.getComponent(0), 0, 1);
+//        }
+//
+//        try {
+//            FileIO.write("./example.proj", pModel);
+//            System.out.println(FileIO.read("./example.proj").toString());
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
---
+++ src/view/Windows.java
@@ -0,0 +1,15 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Windows {
+
+    JFrame popWindow;
+    JPanel panel;
+
+    public Windows() {
+        this.popWindow = new JFrame();
+        this.panel = new JPanel(new BorderLayout());
+    }
+}
